bea4567e2d91a0f39ac79a75c814b86d75940c7f
Mohammad Itani <mohammad.itani1999@hotmail.com>
2024-11-03 18:54:19 +0200

    program.cs shit

--- TasksManager/Program.cs
+++ TasksManager/Program.cs
@@ -1,11 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TasksManager.BackgroundServices;
 using BAL.IServices;
 using BAL.Services;
 using COMMON;
 using DAL.DapperAccess;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using TasksManager.BackgroundServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,11 +27,9 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
 });
 
 builder.Services.AddAuthorization();
-
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.Configure<Configuration>(builder.Configuration.GetSection("Configuration"));
 
 builder.Services.AddTransient<DapperAccess>();
@@ -39,26 +40,22 @@ builder.Services.AddScoped<IUserService, UserService>();
 
 var app = builder.Build();
 
+// Bind to the PORT environment variable required by Heroku
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080"; // Default to 8080 if PORT not set
+app.Urls.Add($"http://*:{port}");
+
 // Configure the HTTP request pipeline.
-
-// Remove HTTPS redirection middleware
 // app.UseHttpsRedirection();
-
 app.UseCors(x => x
     .AllowAnyOrigin()
     .AllowAnyMethod()
     .AllowAnyHeader());
 
-// Enable Swagger only in development (optional)
-// if (app.Environment.IsDevelopment())
-// {
+// Enable Swagger (can be limited to development if desired)
 app.UseSwagger();
 app.UseSwaggerUI();
-// }
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
